7379e2678d4be46a2245aadc0f7b56f558eab5f5
Steven Murray <steven.murray@curtin.edu.au>
2022-08-23 23:23:42 -0700

    slightly smaller fontsizes

--- notebook_utils.py
+++ notebook_utils.py
@@ -12,16 +12,20 @@ double_width = 7.03   # inches
 page_height = 9.43869 # inches
 full_page = (double_width, page_height)
 half_page = (single_width, page_height*single_width / double_width)
+CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
+                  '#f781bf', '#dede00', 
+                  '#a65628', '#984ea3',
+                  '#e41a1c',  '#999999',]
 
 def setup_mpl():
     plt.rcParams['axes.facecolor'] = 'white'
     plt.rcParams['figure.facecolor'] = 'white'
     plt.rcParams['figure.dpi'] = 300
-    plt.rcParams['font.size']= 10
-    plt.rcParams['axes.labelsize'] = 12
-    plt.rcParams['xtick.labelsize'] = 10
-    plt.rcParams['ytick.labelsize'] = 10
-    plt.rcParams['legend.fontsize'] = 11
+    plt.rcParams['font.size']= 9
+    plt.rcParams['axes.labelsize'] = 9
+    plt.rcParams['xtick.labelsize'] = 9
+    plt.rcParams['ytick.labelsize'] = 9
+    plt.rcParams['legend.fontsize'] = 9
     plt.rcParams['hatch.linewidth'] = 0.7
 
 
